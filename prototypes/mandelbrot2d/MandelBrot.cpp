c6b30348b75ca02d51dc43b610916dea763d92fa
VIOLLET ROBIN p1803835 <robin.viollet@etu.univ-lyon1.fr>
2019-10-14 14:25:02 +0200

    Added object MandelBrot

---
+++ prototypes/mandelbrot2d/MandelBrot.cpp
@@ -0,0 +1,178 @@
+//
+// Created by paragoumba on 08/10/2019.
+//
+
+#include <SDL2/SDL_events.h>
+#include <thread>
+#include <SDL2/SDL.h>
+
+#include "MandelBrot.hpp"
+
+MandelBrot::MandelBrot(){
+
+    window = SDL_CreateWindow("MandelBrot", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, MandelBrot::WIDTH, MandelBrot::HEIGHT, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    pixel.w = pixel.h = 1;
+
+    clear();
+
+    calculateImage_x();
+    calculateImage_y();
+
+}
+
+void MandelBrot::setupThread(){
+
+    std::thread nouveauThread(&MandelBrot::calculate, this);
+    nouveauThread.detach();
+
+}
+
+void MandelBrot::setupControls() {
+
+    std::thread controlThread(&MandelBrot::controls, this);
+
+    controlThread.detach();
+
+}
+
+void MandelBrot::calculate(){
+
+    while (running){
+
+        for (int x = 0; (float) x < image_x && x < WIDTH; ++x) {
+            for (int y = 0; (float) y < image_y && y < HEIGHT; ++y) {
+
+                float c_r = (float) x / (float) zoom + x1;
+                float c_i = (float) y / (float) zoom + y1;
+
+                float z_r = 0;
+                float z_i = 0;
+
+                int i = 0;
+
+                do {
+
+                    float tmp = z_r;
+
+                    z_r = z_r * z_r - z_i * z_i + c_r;
+                    z_i = 2 * z_i * tmp + c_i;
+                    i++;
+
+                } while (z_r * z_r + z_i * z_i < 4 && i < iteration_max);
+
+                if (i == iteration_max) {
+
+                    gridMutex.lock();
+                    grid[x][y].r = 255;
+                    grid[x][y].g = 255;
+                    grid[x][y].b = 255;
+                    gridMutex.unlock();
+
+                }
+            }
+        }
+    }
+}
+
+void MandelBrot::display(){
+
+    while (running){
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        clear();
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        gridMutex.lock();
+
+        for (int x = 0; x < WIDTH; ++x) {
+            for (int y = 0; y < HEIGHT; ++y) {
+
+                pixel.x = x;
+                pixel.y = y;
+                SDL_SetRenderDrawColor(renderer, grid[x][y].r, grid[x][y].g, grid[x][y].b, grid[x][y].a);
+                SDL_RenderFillRect(renderer, &pixel);
+
+            }
+        }
+
+        gridMutex.unlock();
+
+        SDL_RenderPresent(renderer);
+
+    }
+
+}
+
+void MandelBrot::clear(){
+
+    for (auto& i : grid) {
+        for (auto& j : i) {
+
+            j.r = 0;
+            j.g = 0;
+            j.b = 0;
+            j.a = 255;
+
+        }
+    }
+}
+
+void MandelBrot::controls(){
+
+    SDL_Event event;
+
+    while (running){
+
+        while (SDL_PollEvent(&event)){
+
+            switch (event.type){
+
+                case SDL_QUIT:
+                    running = false;
+                    break;
+
+                case SDL_KEYUP:
+                    if (event.key.keysym.sym == SDLK_ESCAPE) {
+
+                        running = false;
+
+                    } else if (event.key.keysym.sym == SDLK_UP){
+
+                        zoom += 10;
+                        calculateImage_x();
+                        calculateImage_y();
+
+                    }
+
+                    break;
+
+                default:
+                    break;
+
+            }
+        }
+    }
+}
+
+void MandelBrot::calculateImage_x(){
+
+    image_x = (x2 - x1) * (float) zoom;
+
+}
+
+void MandelBrot::calculateImage_y(){
+
+    image_y = (y2 - y1) * (float) zoom;
+
+}
+
+MandelBrot::~MandelBrot(){
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+}
---
+++ prototypes/mandelbrot2d/MandelBrot.hpp
@@ -0,0 +1,48 @@
+//
+// Created by paragoumba on 08/10/2019.
+//
+
+#ifndef MANDELBULB_MANDELBROT_HPP
+#define MANDELBULB_MANDELBROT_HPP
+
+#include <mutex>
+#include <SDL2/SDL_render.h>
+
+class MandelBrot {
+public:
+    static const int WIDTH = 1000;
+    static const int HEIGHT = 1000;
+
+    MandelBrot();
+
+    void setupThread();
+    void setupControls();
+
+    void calculate();
+    void display();
+    void clear();
+    void controls();
+    void calculateImage_x();
+    void calculateImage_y();
+
+    ~MandelBrot();
+
+private:
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    SDL_Color grid[WIDTH][HEIGHT]{};
+    bool running = true;
+    int zoom = 100;
+    int iteration_max = 50;
+    float x1 = -2.1f;
+    float x2 = 0.6f;
+    float y1 = -1.2f;
+    float y2 = 1.2f;
+    float image_x{};
+    float image_y{};
+    SDL_Rect pixel{};
+    std::mutex gridMutex;
+};
+
+#endif //MANDELBULB_MANDELBROT_HPP
