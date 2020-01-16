f9c649e47bb95f3ebb494850ae6de0fbc5a13cdb
VIOLLET ROBIN p1803835 <robin.viollet@etu.univ-lyon1.fr>
2020-01-16 13:30:55 +0100

    Added Material and fixed a few mistakes

--- src/engine/Mesh.hpp
+++ src/engine/Mesh.hpp
@@ -5,18 +5,23 @@
 #include <memory>
 
 #include "Material.hpp"
+#include "Object.hpp"
 
-class Mesh {
+class Mesh : public Object {
 private:
     unsigned int vaoId{};
     std::vector<unsigned int> vboIds;
     int verticesNumber{};
+
+    MaterialPtr material = nullptr;
 
 public:
     Mesh(const float vertices[], unsigned long verticesNumber, const float texCoords[], unsigned long texCoordsNumber,
          const unsigned int indices[], unsigned long indicesNumber);
 
     void render() const;
+
+    void setMaterial(MaterialPtr _material);
 
     ~Mesh();
 };
