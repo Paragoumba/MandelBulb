dbbd2cab748e28a486fce337fe82b6cd0f91ad68
VIOLLET ROBIN p1803835 <robin.viollet@etu.univ-lyon1.fr>
2020-01-16 13:30:12 +0100

    Added forced vsync + now clears the screen

--- src/engine/GameEngine.cpp
+++ src/engine/GameEngine.cpp
@@ -1,3 +1,5 @@
+#include <chrono>
+
 #include "GameEngine.hpp"
 
 GameEngine::GameEngine() : window("Mandelbulb Renderer", 1920, 1080){
@@ -8,7 +10,15 @@ GameEngine::GameEngine() : window("Mandelbulb Renderer", 1920, 1080){
 
 void GameEngine::loop(){
 
+    timespec waitingTime{0, 0};
+    long waitingTimeNano = (long) (1.0 / 60 * 1'000'000'000.0);
+
     while (!window.shouldClose()){
+
+        glClearColor(std::cos(glfwGetTime()), 255, std::sin(glfwGetTime()), 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        auto loopStart = std::chrono::high_resolution_clock::now();
 
         if (window.getKey(GLFW_KEY_ESCAPE) == GLFW_PRESS){
 
@@ -20,8 +30,11 @@ void GameEngine::loop(){
         game.update();
         game.render();
 
-        glfwPollEvents();
+        waitingTime.tv_nsec = waitingTimeNano - std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - loopStart).count();
+        nanosleep(&waitingTime, nullptr);
+
         window.swapBuffers();
+        glfwPollEvents();
 
     }
 }
