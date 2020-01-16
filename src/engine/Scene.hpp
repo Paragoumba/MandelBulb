42be85f0fca37c033b4d5a6bb30ebe737254f346
VIOLLET ROBIN p1803835 <robin.viollet@etu.univ-lyon1.fr>
2020-01-16 13:26:52 +0100

    Implemented Scene

--- src/engine/Scene.hpp
+++ src/engine/Scene.hpp
@@ -1,9 +1,22 @@
 #ifndef PTUT_SCENE_HPP
 #define PTUT_SCENE_HPP
 
+#include <vector>
+
 #include "Shader.hpp"
+#include "Mesh.hpp"
 
 class Scene {
+private:
+    std::vector<MeshPtr> meshes;
+    MeshPtr cube;
+
+public:
+    Scene();
+
+    void addMesh(MeshPtr& mesh);
+    [[nodiscard]] std::vector<MeshPtr> getMeshes() const;
+    [[nodiscard]] MeshPtr getCube() const;
 };
 
 #endif //PTUT_SCENE_HPP
