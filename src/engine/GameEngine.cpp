851171da98dbc352e8722ce1d3dab2ede707c3fe
VIOLLET ROBIN p1803835 <robin.viollet@etu.univ-lyon1.fr>
2020-01-19 19:06:01 +0100

    Added FPS count in title

--- src/engine/GameEngine.cpp
+++ src/engine/GameEngine.cpp
@@ -2,7 +2,7 @@
 
 #include "GameEngine.hpp"
 
-GameEngine::GameEngine() : window("Mandelbulb Renderer", 1920, 1080){
+GameEngine::GameEngine() : window(appName, 1920, 1080){
 
     game.init();
 
@@ -12,6 +12,8 @@ void GameEngine::loop(){
 
     timespec waitingTime{0, 0};
     long waitingTimeNano = (long) (1.0 / 60 * 1'000'000'000.0);
+    int i = 0;
+    double start = glfwGetTime();
 
     while (!window.shouldClose()){
 
@@ -30,11 +32,24 @@ void GameEngine::loop(){
         game.update();
         game.render();
 
-        waitingTime.tv_nsec = waitingTimeNano - std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - loopStart).count();
+        waitingTime.tv_nsec = waitingTimeNano -
+                std::chrono::duration_cast<std::chrono::nanoseconds>(
+                        std::chrono::high_resolution_clock::now() - loopStart
+                        ).count();
         nanosleep(&waitingTime, nullptr);
+
+        if (glfwGetTime() - start >= 1){
+
+            window.setTitle((std::string(appName) + separator + std::to_string(i) + "FPS").c_str());
+            start = glfwGetTime();
+            i = 0;
+
+        }
 
         window.swapBuffers();
         glfwPollEvents();
+
+        ++i;
 
     }
 }
