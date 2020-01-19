037673e9830fbae67430fe97a8cee28915593e00
VIOLLET ROBIN p1803835 <robin.viollet@etu.univ-lyon1.fr>
2020-01-19 19:01:01 +0100

    Added unit tests

---
+++ tests/cmake-build-debug/CMakeFiles/Tests.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tests.dir/__/lib/glad.c.o"
+  "CMakeFiles/Tests.dir/__/src/engine/Camera.cpp.o"
+  "CMakeFiles/Tests.dir/__/src/engine/Object.cpp.o"
+  "CMakeFiles/Tests.dir/__/src/engine/Transformation.cpp.o"
+  "CMakeFiles/Tests.dir/__/src/engine/Window.cpp.o"
+  "CMakeFiles/Tests.dir/__/src/engine/exceptions/WindowException.cpp.o"
+  "CMakeFiles/Tests.dir/main.cpp.o"
+  "Tests"
+  "Tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
