0ee2105a712494396c53412eed4e9ee97865f437
daniel <daniel.ovejero@etu.univ-lyon1.fr>
2020-01-20 10:37:32 +0100

    Merge branch 'mcv' of /home/daniel/Documents/mandelbulb with conflicts.

---
+++ tests/cmake-build-debug/CMakeFiles/Tests.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/daniel/Documents/mandelbulb/lib/glad.c" "/home/daniel/Documents/mandelbulb/tests/cmake-build-debug/CMakeFiles/Tests.dir/__/lib/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daniel/Documents/mandelbulb/src/engine/Camera.cpp" "/home/daniel/Documents/mandelbulb/tests/cmake-build-debug/CMakeFiles/Tests.dir/__/src/engine/Camera.cpp.o"
+  "/home/daniel/Documents/mandelbulb/src/engine/Object.cpp" "/home/daniel/Documents/mandelbulb/tests/cmake-build-debug/CMakeFiles/Tests.dir/__/src/engine/Object.cpp.o"
+  "/home/daniel/Documents/mandelbulb/src/engine/Transformation.cpp" "/home/daniel/Documents/mandelbulb/tests/cmake-build-debug/CMakeFiles/Tests.dir/__/src/engine/Transformation.cpp.o"
+  "/home/daniel/Documents/mandelbulb/src/engine/Window.cpp" "/home/daniel/Documents/mandelbulb/tests/cmake-build-debug/CMakeFiles/Tests.dir/__/src/engine/Window.cpp.o"
+  "/home/daniel/Documents/mandelbulb/src/engine/exceptions/WindowException.cpp" "/home/daniel/Documents/mandelbulb/tests/cmake-build-debug/CMakeFiles/Tests.dir/__/src/engine/exceptions/WindowException.cpp.o"
+  "/home/daniel/Documents/mandelbulb/tests/main.cpp" "/home/daniel/Documents/mandelbulb/tests/cmake-build-debug/CMakeFiles/Tests.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
