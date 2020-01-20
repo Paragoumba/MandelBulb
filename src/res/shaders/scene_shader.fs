e54e5b03be504dc083abb4c3e6e953b2d9a4bf7b
VIOLLET ROBIN p1803835 <robin.viollet@etu.univ-lyon1.fr>
2020-01-20 12:26:12 +0100

    Added WAVES

--- src/res/shaders/scene_shader.fs
+++ src/res/shaders/scene_shader.fs
@@ -8,9 +8,9 @@ uniform sampler2D texture1;
 
 void main(){
 
-    if (gl_FragCoord.x < 1920 / 3.55 || gl_FragCoord.x > 1920 - 1920 / 3.55){
+    if (gl_FragCoord.x < (sin(gl_FragCoord.y / 10) + 1) * 10 + 1920 / 3.55 || gl_FragCoord.x > (sin(gl_FragCoord.y / 10) + 1) * 10 + 1920 - 1920 / 3.55){
 
-        if (gl_FragCoord.y > 1080 / 2){
+        if (gl_FragCoord.y > (sin(gl_FragCoord.x / 10) + 1) * 10 + 1080 / 2){
 
             FragColor = vec4(1.0, 1.0, 0.0, 1.0);
 
@@ -22,7 +22,7 @@ void main(){
 
     } else {
 
-        if (gl_FragCoord.y > 1080 / 2){
+        if (gl_FragCoord.y > sin(gl_FragCoord.x / 10) * 10 + 1 * 1080 / 2){
 
             FragColor = vec4(0.0, 1.0, 0.0, 1.0);
 
